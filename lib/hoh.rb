83d5d5b6fb14aab5cce17580489798d63e254b59
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-19 23:11:41 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,3 +47,15 @@ def naming_system
   return animals
 end
 
+animals = {
+   :label=> "Kingdom",
+   :sub_category=> {
+     :label=> "Phylum",
+     :sub_category=> {
+       :label=> "Class",
+       :sub_category=> {
+        :label=> "Order"
+       }
+     }
+   }
+ }
