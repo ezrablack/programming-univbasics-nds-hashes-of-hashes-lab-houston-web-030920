7d9d381a9320cf96e677b4e345156ba0a16fe75f
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-19 23:14:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,15 +4,15 @@
 #
 # The tests will guide your construction.
 #
-PORTION_1 = {
-   label: "Kingdom",
-   sub_category: {
-     label: "Phylum",
-     sub_category: {
-       label: "Class"
-     }
-   }
- }
+#PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
 # 
 # PORTION_2 = {
 #   label: "Order"
@@ -31,6 +31,15 @@ PORTION_1 = {
 # }
 
 def naming_system
+  PORTION_1 = {
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+     }
+   }
+ }
   animals = { 
     PORTION_1
   }
